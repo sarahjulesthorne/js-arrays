cec00d8d9266010c7d9f60998529493d86cd8e9c
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-03-05 19:30:33 -0600

    pushing functions

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 console.log(`Hello?`);
+const dinosaurs = [
+    {
+        dinoType: 'long-neck',
+    },
+{
+    dinoType: 'sharp teeth',
+},
+{
+    dinoType: 'winged',
+}];
+const printToDom = (divId, textString) => {
+const selectedDiv = document.getElementById(divId);
+selectedDiv.innerHTML = textString;
+};
+const buildDinosaurs = () => {
+    let domString = '';
+for (let i = 0; i < dinosaurs.length; i++) {
+//console.log(dinosaurs[i].dinoType);
+domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
+
+}
+printToDom('dino-barn', domString);
+};
+const init =    () => {
+buildDinosaurs();
+};
+init();
