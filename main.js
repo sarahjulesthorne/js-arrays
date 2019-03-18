6721b9731596baeb2428444bc459bb6945a57aea
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-03-18 14:12:19 -0500

    pushing final JavaScript with proper indentation

--- main.js
+++ main.js
@@ -4,43 +4,42 @@ We then worked on the next array somewhat more by ourselves, although still with
 We recreated another string-builder function, which looped over the assignments array, and printed cards of fake assignments to the DOM.*/
 
 //Array for practicing looping over arrays
-const dinosaurs = [
-    {
+const dinosaurs = [{
         dinoType: 'long-neck',
         name: 'Fluffy',
     },
-{
-    dinoType: 'sharp teeth',
-    name: 'Goober',
-},
-{
-    dinoType: 'winged',
-    name: 'Nice',
-}];
+    {
+        dinoType: 'sharp teeth',
+        name: 'Goober',
+    },
+    {
+        dinoType: 'winged',
+        name: 'Nice',
+    }
+];
 
 //Function which selects div by id from HTML and prints text to it
 const printToDom = (divId, textString) => {
-const selectedDiv = document.getElementById(divId);
-selectedDiv.innerHTML = textString;
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textString;
 };
 
 //function which loops over the dinosaurs array and pulls information from it into a string of HTML, which the function then prints to the DOM using print function above.
 const buildDinosaurs = () => {
     let domString = '';
-for (let i = 0; i < dinosaurs.length; i++) {
-//console.log(dinosaurs[i].dinoType);
-domString += `<div class='dinosaur'>`;
-domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
-domString += `<p>${dinosaurs[i].name}</p>`;
-domString += `</div>`;
+    for (let i = 0; i < dinosaurs.length; i++) {
+        //console.log(dinosaurs[i].dinoType);
+        domString += `<div class='dinosaur'>`;
+        domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
+        domString += `<p>${dinosaurs[i].name}</p>`;
+        domString += `</div>`;
 
-}
-printToDom('dino-barn', domString);
+    }
+    printToDom('dino-barn', domString);
 };
 
 //Array of fake assignments for practice with looping over arrays
-const assignments = [
-    {
+const assignments = [{
         title: 'product-cards',
         dueDate: '03/05/2019',
         topic: 'html/css',
@@ -79,22 +78,23 @@ const assignments = [
 
 //function which loops over the assignments array, pulls information from that array, builds  that information into a string of HTML, and prints that string to the DOM.
 const buildassignmentCards = () => {
-let domString = '';
-for (let i = 0; i < assignments.length; i++) {
-domString += `<div class='card'>`;
-domString += `<h1 class='title'>${assignments[i].title}</h1>`;
-domString += `<p class='due-date'>${assignments[i].dueDate}</p>`;
-domString += `<p>Get the assignment <a href=${assignments[i].assignmentURL}>here</a></p>`;
-domString += `<p>Topic: ${assignments[i].topic}</p>`;
-domString += `<p>Notes: ${assignments[i].notes}</p>`;domString += `</div>`;
-domString += `</div>`;
-}
-printToDom('assignments', domString);
+    let domString = '';
+    for (let i = 0; i < assignments.length; i++) {
+        domString += `<div class='card'>`;
+        domString += `<h1 class='title'>${assignments[i].title}</h1>`;
+        domString += `<p class='due-date'>${assignments[i].dueDate}</p>`;
+        domString += `<p>Get the assignment <a href=${assignments[i].assignmentURL}>here</a></p>`;
+        domString += `<p>Topic: ${assignments[i].topic}</p>`;
+        domString += `<p>Notes: ${assignments[i].notes}</p>`;
+        domString += `</div>`;
+        domString += `</div>`;
+    }
+    printToDom('assignments', domString);
 };
 
 //Initialize function which calls page load functions
-const init =    () => {
-buildDinosaurs();
-buildassignmentCards(); 
+const init = () => {
+    buildDinosaurs();
+    buildassignmentCards();
 };
 init();
